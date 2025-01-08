73d695fc6bb41be6f069b218fe9565d11a776c95
suvorova.a <suvorova.a@brainrocket.com>
2025-01-08 17:08:38 +0200

    need to fix and redo the last 2 buttons

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,39 @@
-$(function() {
+$(function () {
+  const card = document.getElementById("btn");
+  const card1 = document.getElementById("card");
 
-	// Custom JS
-
+  card.addEventListener("mousemove", (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) * 5;
+    let yAxis = (window.innerHeight / 2 - e.pageY) * 5;
+    card.style.transform =
+      "perspective(1000px) translateY(" +
+      xAxis +
+      "px)" +
+      "translateX(" +
+      yAxis +
+      "px)";
+  });
+  card.addEventListener("click", (e) => {
+    e.preventDefault();
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 1;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 1;
+    card.style.transform =
+      "perspective(1000px) translateX(" +
+      xAxis +
+      "px)" +
+      "translateY(" +
+      yAxis +
+      "px)";
+  });
+  card.addEventListener("mouseleave", (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 1;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 1;
+    card.style.transform =
+      "perspective(1000px) translateY(" +
+      "0" +
+      "px)" +
+      "translateX(" +
+      "0" +
+      "px)";
+  });
 });
